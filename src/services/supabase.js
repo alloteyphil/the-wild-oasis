1b9da96de7739b6cffbdd7e17d24622f3a57de1e
“Philip <alloteyphilip@gmail.com>
2024-02-15 09:05:52 +0000

    initial commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://ensnatkqbqhnmzuhrdho.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuc25hdGtxYnFobm16dWhyZGhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxNTM1NDAsImV4cCI6MjAyMjcyOTU0MH0.MdF2U6gL3POWwf0XrGJ9N1k6kgLCHbzD6Un3BW1JE94";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
