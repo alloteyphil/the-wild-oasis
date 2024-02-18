512e67f816d7528a9fe311a8c2666d43348e5ba4
“Philip <alloteyphilip@gmail.com>
2024-02-18 22:21:07 +0000

    Added fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+// Load environment variables
+dotenv.config();
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    define: {
+      // Define environment variables that you want to access in your application code
+      "process.env.REACT_APP_API_KEY": JSON.stringify(
+        process.env.VITE_SUPABASE_API_KEY
+      ),
+    },
+  };
 });
