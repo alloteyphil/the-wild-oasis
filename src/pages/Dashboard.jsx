3e6019bf972fe93c11f0d5818e7f65490037f8ee
Philip Allotey <philipallotey@Philips-MacBook-Pro.local>
2024-05-20 23:54:49 +0000

    Added new sections

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import Uploader from "../data/Uploader.jsx";
 import DashboardFilter from "../features/dashboard/DashboardFilter.jsx";
 import DashboardLayout from "../features/dashboard/DashboardLayout.jsx";
 import Heading from "../ui/Heading";
@@ -9,6 +10,7 @@ function Dashboard() {
       <Row type="horizontal">
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
+        <Uploader />
       </Row>
       <DashboardLayout />
     </>
